551b6fafa3609ffb7106f61bcba63e27a201dbaa
yingpinglin <148830067+yingpinglin@users.noreply.github.com>
2023-12-05 12:44:51 -0500

    Update CS54.cpp

--- CS54.cpp
+++ CS54.cpp
@@ -1,7 +1,7 @@
 //Name: Ying Ping Lin 
 //Email: yingping.lin69@myhunter.cuny.edu 
-//Date: Dec 4, 2023 
-//This program prints program 53
+//Date: Dec 5, 2023 
+//This program prints program 54
     
 #include <iostream>
 using namespace std;
