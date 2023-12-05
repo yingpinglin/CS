e98dc59d04e55d4d7959a6484d89345dfafaa694
yingpinglin <148830067+yingpinglin@users.noreply.github.com>
2023-12-05 12:40:20 -0500

    CS53.cpp
    Program outputs "Hello, World!" with explanation

--- CS53.cpp
+++ CS53.cpp
@@ -3,11 +3,10 @@
 //Date: Dec 4, 2023 
 //This program prints program 53
     
-#include <iostream>
-using namespace std;
-
-int main(){
-    cout <<"Hello, World!";
-    cout << "Hello, C++!";
-    return 0;
+#include <iostream>               //header file:performs input and output operations in C++ (<>=system header file)
+using namespace std;              //tells compiler to use std(standard) namespace like cout(common C++ library feature)
+int main(){                      //line8-12 = main function(entry point) <--execution of program starts here
+    cout <<"Hello, World!"\n;    //use cout object to output string "Hello, World!"(similar to line10)
+    cout << "Hello, C++!"\n;    // << operator is used to send data to cout
+    return 0;                    //exits main function & returns value 0 to operating system, indicating program executed successfully
     }
