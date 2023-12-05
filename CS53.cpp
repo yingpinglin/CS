79a7394839d92a75a5f56c39afba121943125b2f
yingpinglin <148830067+yingpinglin@users.noreply.github.com>
2023-12-04 21:53:21 -0500

    CS53.cpp

---
+++ CS53.cpp
@@ -0,0 +1,13 @@
+//Name: Ying Ping Lin 
+//Email: yingping.lin69@myhunter.cuny.edu 
+//Date: Dec 4, 2023 
+//This program prints program 53
+    
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout <<"Hello, World!";
+    cout << "Hello, C++!";
+    return 0;
+    }
