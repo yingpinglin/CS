e0244feda9ce74679e938d045a5023773dbafda0
yingpinglin <148830067+yingpinglin@users.noreply.github.com>
2023-12-05 21:00:01 -0500

    Update CS55.cpp

--- CS55.cpp
+++ CS55.cpp
@@ -1,16 +1,16 @@
 //Name: Ying Ping Lin 
 //Email: yingping.lin69@myhunter.cuny.edu 
 //Date: Dec 5, 2023 
-//This program prints program 54
+//This program prints program 55
     
 #include <iostream>
 using namespace std;
 
 int main(){
-  int km, miles;
+  float km, miles;
   cout <<"Enter number of kilometers: ";
   cin >> km;
-  miles = 0,621371 * km;
+  miles = 0.621371 * km;
   cout << "Covert kilmeters to miles: " << miles << endl;
   return 0;
 }
