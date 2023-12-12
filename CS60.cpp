60f2091f819388f88bb23429a36d6ca5651812b8
yingpinglin <148830067+yingpinglin@users.noreply.github.com>
2023-12-11 20:08:45 -0500

    Update CS60.cpp

--- CS60.cpp
+++ CS60.cpp
@@ -1,7 +1,24 @@
 //Name: Ying Ping Lin 
 //Email: yingping.lin69@myhunter.cuny.edu 
 //Date: Dec 11, 2023 
-//This program prints program 59
+//This program prints program 60
+// Write a C++ program that asks the user for a whole number between -31 and 31 and prints out the number in "two's complement" notation, using the following algorithm:
+// 1)Ask the user for a number, n.
+// 2)If the number is negative, print a 1 and let x = 32 + n.
+// 3)If the number is not negative, print a 0 and let x = n.
+// 4)Let b = 16.
+// 5)While b > 0.5:
+//     1)If x >= b then print 1, otherwise print 0
+//     2)Let x be the remainder of dividing x by b.
+//     3)Let b be b/2.
+// 6)Print a new line ('\n').
+
+// A sample run:
+// Enter a number:  8
+// 001000
+// Another run:
+// Enter a number: -1
+// 111111
 #include <iostream>
 using namespace std;
 
