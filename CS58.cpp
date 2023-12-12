cc9bfaaa2a247239152278cb0660c92ee7fcbd28
yingpinglin <148830067+yingpinglin@users.noreply.github.com>
2023-12-11 20:06:32 -0500

    Update CS58.cpp

--- CS58.cpp
+++ CS58.cpp
@@ -2,6 +2,15 @@
 //Email: yingping.lin69@myhunter.cuny.edu 
 //Date: Dec 11, 2023 
 //This program prints program 58
+//Write a C++ program that asks the user for a year, and continue asking until the number entered that is 2018 or earlier.
+
+// A sample run:
+// Enter year: 2023
+// Year must be 2018 or earlier
+// Enter year: 2025
+// Year must be 2018 or earlier
+// Enter year: 2000
+// You entered:  2000
 #include <iostream>
 using namespace std;
 
