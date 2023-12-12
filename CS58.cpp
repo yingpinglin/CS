93068111f88dd3c829a83d15ca9095e04cc58306
yingpinglin <148830067+yingpinglin@users.noreply.github.com>
2023-12-11 20:18:35 -0500

    Update CS58.cpp
    .

--- CS58.cpp
+++ CS58.cpp
@@ -1,7 +1,5 @@
 //Name: Ying Ping Lin 
 //Email: yingping.lin69@myhunter.cuny.edu 
-//Date: Dec 11, 2023 
-//This program prints program 58
 //Write a C++ program that asks the user for a year, and continue asking until the number entered that is 2018 or earlier.
 
 // A sample run:
@@ -11,14 +9,17 @@
 // Year must be 2018 or earlier
 // Enter year: 2000
 // You entered:  2000
+
 #include <iostream>
 using namespace std;
 
-int main() {
+int main() 
+{
   int year;
   cout << "Enter a year: ";
   cin >> year;
-  while (year > 2018) {
+  while (year > 2018) 
+  {
     cout << "Year must be 2018 or after 2018.";
     cout << "\n Enter year: ";
     cin >> year;
