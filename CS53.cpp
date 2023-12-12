bdd69169c8c70c37609104cbabee70989fb2ce0d
yingpinglin <148830067+yingpinglin@users.noreply.github.com>
2023-12-11 20:00:54 -0500

    Update CS53.cpp

--- CS53.cpp
+++ CS53.cpp
@@ -2,7 +2,7 @@
 //Email: yingping.lin69@myhunter.cuny.edu 
 //Date: Dec 4, 2023 
 //This program prints program 53
-    
+//Write a C++ program that prints "Hello, World!" and "Hello, C++!" in two separate lines to the screen.   
 #include <iostream>               //header file:performs input and output operations in C++ (<>=system header file)
 using namespace std;              //tells compiler to use std(standard) namespace like cout(common C++ library feature)
 int main(){                      //line8-12 = main function(entry point) <--execution of program starts here
