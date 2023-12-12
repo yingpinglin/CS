2db44b7363fb9b410ed840a8e4a5a736b3f3b5fa
yingpinglin <148830067+yingpinglin@users.noreply.github.com>
2023-12-11 20:16:39 -0500

    Update CS56.cpp
    .

--- CS56.cpp
+++ CS56.cpp
@@ -1,7 +1,5 @@
 //Name: Ying Ping Lin 
 //Email: yingping.lin69@myhunter.cuny.edu 
-//Date: Dec 5, 2023 
-//This program prints program 56
 //Write a C++ program program that asks the user for a number and draws a triangle of that height and width using 'character graphics'.
 
 // A sample run:
@@ -17,15 +15,19 @@
 // *
 // **
 // ***
+
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int num;
     cout << "Enter a number: ";
     cin >> num;
-    for (int i = 1; i <= num; i++){
-        for (int j = 1; j <= i; j++){
+    for (int i = 1; i <= num; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
             cout << "*";
         }
         cout << endl;
